b7a5ca70470ee9fa182d30ea3726c6a1ea1367ce
Liopin <contact.pinheiro.lionel@gmail.com>
2020-10-01 19:26:48 +0200

    amplify setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StoreModule } from "./store/store.module";
 
+/** import AmplifyUIAngularModule */
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
+/** import { AppRoutingModule } from './app-routing.module'; */
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    [BrowserModule, StoreModule]
+    [BrowserModule, StoreModule,AmplifyUIAngularModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
