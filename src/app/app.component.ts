61df2c5f6d8b2a1c1f00f887ab1e93f138f652e0
Liopin <contact.pinheiro.lionel@gmail.com>
2020-10-01 17:29:46 +0200

    database connected to the front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: "<store></store>",
 })
-export class AppComponent {
-  title = 'SportsStore';
-}
+export class AppComponent {}
