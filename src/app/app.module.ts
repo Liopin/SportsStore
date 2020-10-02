b3eca30393d83a105d9b7dc79e2ba7341c754d57
Liopin <contact.pinheiro.lionel@gmail.com>
2020-10-02 16:57:46 +0200

    routing is functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';  
 import { AppComponent } from './app.component';
-import { StoreModule } from "./store/store.module";
-
+import { StoreModule } from './store/store.module';
+import { StoreComponent } from './store/store.component';
+import { AdminComponent} from './store/admin.component';
 /** import AmplifyUIAngularModule */
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -16,12 +17,19 @@ import { AppRoutingModule } from './app-routing.module'
   declarations: [
     AppComponent,
   ],
-  imports: 
-    [BrowserModule, 
+  imports: [
+    BrowserModule, 
     StoreModule,
     AmplifyUIAngularModule,
     FormsModule, 
-    ReactiveFormsModule, AppRoutingModule],
+    ReactiveFormsModule, 
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: "store", component: StoreComponent},
+      {path: "admin", component: AdminComponent},
+      {path: "**", redirectTo: "/store"}
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from "@angular/core";
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { StoreComponent } from "./store.component";
+import { AdminComponent } from "./admin.component";
 
 @NgModule({
-    declarations: [StoreComponent],
-    exports: [StoreComponent],
+    declarations: [
+      StoreComponent,
+      AdminComponent
+    ],
+    exports: [
+      StoreComponent,
+      AdminComponent
+    ],
     imports: [
         BrowserModule,
         AmplifyUIAngularModule,
